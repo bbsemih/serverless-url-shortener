8144b505706556f9213af53f6808988104b3fb10
Semih Berkay Öztürk <90466553+semihberkayozturk@users.noreply.github.com>
2023-05-19 00:09:36 +0300

    implement redirect

--- main.go
+++ main.go
@@ -1,37 +1,76 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
+	"net"
+	"net/http"
+	"os"
 
 	"google.golang.org/api/sheets/v4"
 )
 
 func main() {
-	ctx := context.Background()
+	port, addr := os.Getenv("PORT"), os.Getenv("LISTEN_ADDR")
+	if port == "" {
+		port = "8080"
+	}
+	if addr == "" {
+		addr = "localhost"
+	}
+
+	googleSheetsId := os.Getenv("GSHEET_ID")
+	sheetName := os.Getenv("SHEET_NAME")
+
+	srv := &server{
+		googleSheetsId: googleSheetsId,
+		sheetName:      sheetName,
+	}
+
+	http.HandleFunc("/", srv.redirect)
+	//combines host and port into a network address
+	listenAddr := net.JoinHostPort(addr, port)
+	log.Printf("Starting server at %s", listenAddr)
+	err := http.ListenAndServe(listenAddr, nil)
+	log.Fatal(err)
+}
+
+type server struct {
+	googleSheetsId string
+	sheetName      string
+}
+
+func (s *server) redirect(w http.ResponseWriter, r *http.Request) {
+	if s.googleSheetsId == "" {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "No google sheets id provided!")
+		return
+	} else if s.sheetName == "" {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "No sheet name provided!")
+		return
+	}
+
+	ctx := r.Context()
 
 	srv, err := sheets.NewService(ctx)
 	if err != nil {
 		log.Fatalf("Unable to retrieve Sheets client: %v", err)
 	}
 
-	// Prints the names and majors of students in a sample spreadsheet:
-	// https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
-	spreadsheetId := "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms"
-	readRange := "Sheet1!A:B"
-	resp, err := srv.Spreadsheets.Values.Get(spreadsheetId, readRange).Do()
+	log.Println("querying the sheet!")
+	readRange := "!A:B"
+	resp, err := srv.Spreadsheets.Values.Get(s.googleSheetsId, readRange).Do()
 	if err != nil {
 		log.Fatalf("Unable to retrieve data from sheet: %v", err)
 	}
 
 	if len(resp.Values) == 0 {
-		fmt.Println("No data found.")
+		log.Println("No data found.")
 	} else {
-		fmt.Println("Name, Major:")
+		log.Printf("found %d rows of data", len(resp.Values))
 		for _, row := range resp.Values {
-			// Print columns A and E, which correspond to indices 0 and 4.
-			fmt.Printf("%#v", row)
+			fmt.Printf("%#v\n", row)
 		}
 	}
 }
